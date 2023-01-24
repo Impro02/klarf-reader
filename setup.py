dfec76207b59f5ce7f0587173c11c75e0b52a1a5
Maxime MARTIN <maxime.martin@st.com>
2023-01-24 11:09:32 +0100

    fix: update version 0.1.7

--- setup.py
+++ setup.py
@@ -1,16 +1,21 @@
 from setuptools import setup
 
+version = "0.1.7"
+
 setup(
     name="klarf-reader",
-    version="0.1.5",
+    version=version,
     packages=["klarf_reader", "klarf_reader.models", "klarf_reader.readers"],
     install_requires=[
         "numpy",
     ],
+    license="MIT",
     author="Maxime MARTIN",
     author_email="maxime.martin02@hotmail.fr",
     description="A project to parse klarf file and get klarf content as dataclass",
     url="https://github.com/Impro02/klarf_reader",
+    download_url="https://github.com/Impro02/klarf_reader/archive/refs/tags/%s.tar.gz"
+    % version,
     classifiers=[
         "Development Status :: 3 - Alpha",
         "Intended Audience :: Developers",
